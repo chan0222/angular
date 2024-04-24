6bfb56d9a1e15d40d9aaaf9b692e3dfe3c3fb541
곽찬영 <gksmftor8745@naver.com>
2024-04-24 15:49:12 +0900

    Init: 초기 화면 세팅

---
+++ src/app/practice1/practice1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice1',
+  standalone: true,
+  imports: [],
+  templateUrl: './practice1.component.html',
+
+})
+export class Practice1Component {
+
+}
