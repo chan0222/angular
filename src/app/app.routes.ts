90e15431d11a910e9b21766aad8e20bb59340c76
곽찬영 <gksmftor8745@naver.com>
2024-04-25 10:44:50 +0900

    Feat: 두번째 dialog 연습 라우터 연결

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,13 @@ export const routes: Routes = [
     {
         path: 'simple-dialog',
         loadComponent:()=> import('./pages/simple-dialog/simple-dialog.component').then((m)=> m.SimpleDialogComponent)
+    },
+    {
+        path: 'practice2',
+       loadComponent:()=> import('./pages/practice2/practice2.component').then((m)=> m.Practice2Component)
+    },
+    {
+        path: 'input-dialog',
+        loadComponent:()=> import('./pages/input-dialog/input-dialog.component').then((m)=> m.InputDialogComponent)
     }
 ];
